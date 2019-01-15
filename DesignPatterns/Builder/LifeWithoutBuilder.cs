dd23d3469fbaa0fc04fed972aacb96d8f849f6be
Cristi Paul <cristimedrea@hotmail.com>
2019-01-15 20:38:15 +0200

    add Builder pattern

---
+++ DesignPatterns/Builder/LifeWithoutBuilder.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+
+namespace Builder
+{
+    class LifeWithoutBuilder
+    {
+        //Builder pattern = when piecewise object construction is complicated, provide an api for doing it succintly
+        //Having an object with 10 constructor arguments is not productive
+        //Opt for piecewise construction
+
+        void LifeWithoutBuilderMethod(string[] args)
+        {
+            var hello = "Hello";
+            var sb = new StringBuilder();
+            sb.Append("<p>");
+            sb.Append(hello);
+            sb.Append("</p>");
+            System.Console.WriteLine(sb);
+
+            var words = new[]
+            {
+                "hello", "world"
+            };
+
+            sb.Clear();
+            sb.Append("<ul>");
+            foreach (var v in words)
+            {
+                sb.AppendFormat("<li>{0}</li>", v);
+            }
+            sb.Append("</ul>");
+            System.Console.WriteLine(sb);
+        }
+
+
+
+        static void LifeWithoutBuilderMain(string[] args)
+        {
+            var hello = "Hello";
+            var sb = new StringBuilder();
+            sb.Append("<p>");
+            sb.Append(hello);
+            sb.Append("</p>");
+            System.Console.WriteLine(sb);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ DesignPatterns/Builder/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Builder
+{
+    //solution
+    public class HtmlElement {
+        public string Name, Text;
+        public List<HtmlElement> Elements = new List<HtmlElement>();
+        private const int indentSize = 2;
+
+        public HtmlElement() { }
+        public HtmlElement(string name, string text) {
+            Name = name ?? throw new Exception();
+            Text= text ?? throw new Exception();
+
+        }
+        private string ToStringImpl(int indent) {
+            var sb = new StringBuilder();
+            var i = new String(' ', indentSize * indent);
+                sb.Append($"{i}<{Name}>");
+            if (!string.IsNullOrWhiteSpace(Text))
+            {
+                sb.Append(new string(' ', indentSize * (indent + 1)));
+                sb.AppendLine(Text);
+            }
+
+            foreach(var e in Elements)
+            {
+                sb.Append(e.ToStringImpl(indent + 1));
+            }
+            return sb.ToString();
+        }
+
+        public override string ToString()
+        {
+            return ToStringImpl(0);
+        }
+    }
+
+    public class HtmlBuilder
+    {
+        HtmlElement root = new HtmlElement();
+        private string rootname;
+        public HtmlBuilder (string rootname)
+        {
+            this.rootname = rootname;
+            root.Name = rootname;
+        }
+
+        public void AddChild(string childName, string childText)
+        {
+            var e = new HtmlElement(childName, childText);
+            root.Elements.Add(e);
+        }
+
+        public override string ToString()
+        {
+            return root.ToString();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new HtmlBuilder("ul");
+            builder.AddChild("li", "hello");
+            builder.AddChild("li", "world");
+            System.Console.WriteLine(builder.ToString());
+        }
+    }
+}
